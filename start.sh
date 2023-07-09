fd383deef20055af837c79226dab1907678081aa
Papp Zoltán <papp.zoltan@infokristaly.hu>
2023-07-09 10:10:11 +0200

    stop and exec, 20.3.1-bookworm-slim

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ~/energymeter-timed-service
-npx ts-node src/index.ts &
+npx ts-node src/index.ts>log.txt 2>error.log.txt  &
 cd ~/energymeter-api
-npx ts-node src/app.ts &
+npx ts-node src/app.ts>log.txt 2>error.log.txt &
 cd ~/energymeter-admin
 npx react-scripts start
